3b077f093ae725172123ab8ec542a7aed3cc670d
MuhammadArham Shaikh <arhamshaikh128@gmail.com>
2025-06-29 19:15:17 +0500

    getVideoComments controller done

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -8,7 +8,53 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 const getVideoComments = asyncHandler(async (req, res) => {
   //TODO: get all comments for a video
   const { videoId } = req.params;
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 5 } = req.query;
+
+  const pageNumber = parseInt(page);
+  const limitNumber = parseInt(limit);
+
+  if (
+    isNaN(pageNumber) ||
+    isNaN(limitNumber) ||
+    pageNumber < 1 ||
+    limitNumber < 1
+  ) {
+    throw new ApiError(400, "Page and limit must be positive integers.");
+  }
+
+  if (!mongoose.isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid video ID format");
+  }
+
+  const video = await Video.findById(videoId);
+  if (!video) {
+    throw new ApiError(400, "video does not exist");
+  }
+
+  // Get total comments count
+  const totalComments = await Comment.countDocuments({ video: videoId });
+
+  const comments = await Comment.find({ video: videoId })
+    .sort({
+      createdAt: -1,
+    })
+    .skip((pageNumber - 1) * limitNumber)
+    .limit(limitNumber)
+    .populate("owner", "username avatar")
+    .lean();
+
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        totalComments,
+        comments,
+        totalPages: Math.ceil(totalComments / limitNumber),
+        currentPage: pageNumber,
+      },
+      "Comments fetched successfully"
+    )
+  );
 });
 
 const addComment = asyncHandler(async (req, res) => {
@@ -43,7 +89,7 @@ const addComment = asyncHandler(async (req, res) => {
   }
 
   await comment.populate([
-    { path: "owner", select: "username" },
+    { path: "owner", select: "username avatar" },
     { path: "video", select: "title" },
   ]);
 
