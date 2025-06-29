ecea972e8e2126793987a28eb49977c6f989f397
MuhammadArham Shaikh <arhamshaikh128@gmail.com>
2025-06-29 20:22:41 +0500

    all tweet controllers done except getUserTweets controller

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -34,10 +34,53 @@ const getUserTweets = asyncHandler(async (req, res) => {
 
 const updateTweet = asyncHandler(async (req, res) => {
   //TODO: update tweet
+  const { tweetId } = req.params;
+  const { content } = req.body;
+
+  if (!mongoose.isValidObjectId(tweetId)) {
+    throw new ApiError(400, "Invalid Tweet ID format");
+  }
+
+  const tweet = await Tweet.findOne({ _id: tweetId, owner: req.user._id });
+
+  if (!tweet) {
+    throw new ApiError(404, "Tweet not found or not owned by you");
+  }
+
+  if (!content || content.trim() === "" || typeof content !== "string") {
+    throw new ApiError(
+      400,
+      "Tweet content is required and must be a valid string"
+    );
+  }
+
+  tweet.content = content;
+  await tweet.save({ validateBeforeSave: false });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, tweet, "Tweet updated successfully"));
 });
 
 const deleteTweet = asyncHandler(async (req, res) => {
   //TODO: delete tweet
+  const { tweetId } = req.params;
+
+  if (!mongoose.isValidObjectId(tweetId)) {
+    throw new ApiError(400, "Invalid Tweet ID format");
+  }
+
+  const tweet = await Tweet.findOne({ _id: tweetId, owner: req.user._id });
+
+  if (!tweet) {
+    throw new ApiError(404, "Tweet not found or not owned by you");
+  }
+
+  await tweet.deleteOne();
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, tweet, "Tweet deleted successfully"));
 });
 
 export { createTweet, getUserTweets, updateTweet, deleteTweet };
